ad8d78fdc14be4b481fc1680fcdd57ef8030b729
Steven Lumos <slumos@yammer-inc.com>
2013-04-01 10:28:40 -0700

    improve seeding of ThreadLocalRandom

--- src/metrics/Support/ThreadLocalRandom.cs
+++ src/metrics/Support/ThreadLocalRandom.cs
@@ -5,17 +5,32 @@ namespace metrics.Support
 {
     class ThreadLocalRandom
     {
-        public static ThreadLocal<System.Random> _random;
+        private static readonly System.Random Seeder = new System.Random();
+        private static readonly ThreadLocal<int> Seed;
+
+        private static ThreadLocal<System.Random> _random;
 
         static ThreadLocalRandom()
         {
-            _random = new ThreadLocal<System.Random>(() => new System.Random(Guid.NewGuid().GetHashCode()));
+            lock (Seeder)
+            {
+                Seed = new ThreadLocal<int>(() => Seeder.Next());
+            }
         }
 
         public double NextNonzeroDouble()
         {
+            EnsureInitialized();
             var r = _random.Value.NextDouble();
             return Math.Max(r, Double.Epsilon);
+        }
+
+        private void EnsureInitialized()
+        {
+            if (_random == null)
+            {
+                _random = new ThreadLocal<System.Random>(() => new System.Random(Seed.Value));
+            }
         }
     }
 }
